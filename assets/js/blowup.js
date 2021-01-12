007641e0749f97f0080d9fb91316a630fb6136c2
wael <w.rammo@gmail.com>
2021-01-12 04:03:47 -0500

    blowup fix

--- assets/js/blowup.js
+++ assets/js/blowup.js
@@ -1,6 +1,6 @@
 $(function ($) {
   $.fn.blowup = function (attributes) {
-    var $element = this
+    var $element = this;
 
     // If the target element is not an image
     if (!$element.is("img")) {
@@ -8,14 +8,14 @@ $(function ($) {
         "%c Blowup.js Error: " + "%cTarget element is not an image.",
         "background: #FCEBB6; color: #F07818; font-size: 17px; font-weight: bold;",
         "background: #FCEBB6; color: #F07818; font-size: 17px;"
-      )
-      return
+      );
+      return;
     }
 
     // Constants
-    var $IMAGE_URL = $element.attr("src")
-    var NATIVE_IMG = new Image()
-    NATIVE_IMG.src = $element.attr("src")
+    var $IMAGE_URL = $element.attr("src");
+    var NATIVE_IMG = new Image();
+    NATIVE_IMG.src = $element.attr("src");
 
     // Default attributes
     var defaults = {
@@ -29,26 +29,31 @@ $(function ($) {
       zIndex: 999999,
       scale: 1,
       customClasses: "",
-    }
+    };
 
     // Update defaults with custom attributes
-    var $options = $.extend(defaults, attributes)
+    var $options = $.extend(defaults, attributes);
 
     // Modify target image
     $element.on("dragstart", function (e) {
-      e.preventDefault()
-    })
-    $element.css("cursor", $options.cursor ? "crosshair" : "none")
+      e.preventDefault();
+    });
+    $element.css("cursor", $options.cursor ? "crosshair" : "none");
 
-    // Create magnification lens element
-    var lens = document.createElement("div")
-    lens.id = "BlowupLens"
+    // check if already exists
+    $blowupLens = $("#BlowupLens");
 
-    // Attack the element to the body
-    $("body").append(lens)
+    if (!$blowupLens.length) {
+      // Create magnification lens element
+      var lens = document.createElement("div");
+      lens.id = "BlowupLens";
 
-    // Updates styles
-    $blowupLens = $("#BlowupLens")
+      // Attack the element to the body
+      $("body").append(lens);
+
+      // Updates styles
+      $blowupLens = $("#BlowupLens");
+    }
 
     $blowupLens.css({
       position: "absolute",
@@ -62,45 +67,45 @@ $(function ($) {
       "border-radius": $options.round ? "50%" : "none",
       "box-shadow": $options.shadow,
       "background-repeat": "no-repeat",
-    })
+    });
 
     // Add custom CSS classes
-    $blowupLens.addClass($options.customClasses)
+    $blowupLens.addClass($options.customClasses);
 
     // Show magnification lens
     $element.mouseenter(function () {
-      $blowupLens.css("display", "block")
-    })
+      $blowupLens.css("display", "block");
+    });
 
     // Mouse motion on image
-    console.log($IMAGE_URL)
-    console.log($element)
-    console.log($element.attr("src"))
+    console.log($IMAGE_URL);
+    console.log($element);
+    console.log($element.attr("src"));
     $element.mousemove(function (e) {
       // Lens position coordinates
-      var lensX = e.pageX - $options.width / 2
-      var lensY = e.pageY - $options.height / 2
+      var lensX = e.pageX - $options.width / 2;
+      var lensY = e.pageY - $options.height / 2;
 
       // Relative coordinates of image
-      var relX = e.pageX - $(this).offset().left
-      var relY = e.pageY - $(this).offset().top
+      var relX = e.pageX - $(this).offset().left;
+      var relY = e.pageY - $(this).offset().top;
 
       // Zoomed image coordinates
       var zoomX = -Math.floor(
         (relX / $element.width()) * (NATIVE_IMG.width * $options.scale) -
           $options.width / 2
-      )
+      );
       var zoomY = -Math.floor(
         (relY / $element.height()) * (NATIVE_IMG.height * $options.scale) -
           $options.height / 2
-      )
+      );
 
-      var backPos = zoomX + "px " + zoomY + "px"
+      var backPos = zoomX + "px " + zoomY + "px";
       var backgroundSize =
         NATIVE_IMG.width * $options.scale +
         "px " +
         NATIVE_IMG.height * $options.scale +
-        "px"
+        "px";
 
       // Apply styles to lens
       $blowupLens.css({
@@ -109,12 +114,12 @@ $(function ($) {
         "background-image": "url(" + encodeURI($IMAGE_URL) + ")",
         "background-size": backgroundSize,
         "background-position": backPos,
-      })
-    })
+      });
+    });
 
     // Hide magnification lens
     $element.mouseleave(function () {
-      $blowupLens.css("display", "none")
-    })
-  }
-})
+      $blowupLens.css("display", "none");
+    });
+  };
+});

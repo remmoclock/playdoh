d10b93217a2548627525f712d96ae84c83772e1f
Ahmad Remmo <aremmo@exaduo.fr>
2021-04-06 12:25:50 +0200

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,24 +24,24 @@ $(function () {
 
   $(".lin").click(function (e) {
     let imgStr = $(".imageDisplay > img")[0].src
-    console.log('lin', imgStr);
+    console.log("lin", imgStr)
     var start = imgStr.indexOf("/assets/img")
     var verso = imgStr.indexOf("-verso")
     var p = imgStr.lastIndexOf(".")
     if (verso < 0) {
-      imgStr = ".." + imgStr.substring(start, p) + "-verso" + imgStr.substring(p)
+      imgStr =
+        ".." + imgStr.substring(start, p) + "-verso" + imgStr.substring(p)
       btnStr = "../assets/img/buttons/button-pro.png"
     } else {
       imgStr = ".." + imgStr.substring(start, verso) + imgStr.substring(p)
       btnStr = "../assets/img/buttons/button-ref.png"
     }
-    console.log('lin', imgStr);
+    console.log("lin", imgStr)
     $(".imageDisplay > img").attr("src", imgStr)
     $(".lin").attr("src", btnStr)
     magnify()
   })
 
- 
   $(".plan1").click(function (e) {
     openShadowBox("../assets/img/plans/planpop1.png")
   })
@@ -83,21 +83,23 @@ $(function () {
     openVideoShadowBox("../assets/img/video3.mp4")
   })
 
-  $(".scrollbox-4 img, .scrollbox-5 img, .scrollbox-6 img, .scrollbox img, .scrollbox-7 img").click(function (e) {
+  $(
+    ".scrollbox-4 img, .scrollbox-5 img, .scrollbox-6 img, .scrollbox img, .scrollbox-7 img"
+  ).click(function (e) {
     openShadowBox(e.target.src)
   })
-  $(".scrollbox ul li:not(:first-child) img").click(function (e) {
-    var str = e.target.src
-    var p1 = str.lastIndexOf("/")
-    var p2 = str.lastIndexOf(".")
-    str =
-      str.substring(0, p1 + 1) +
-      "pop1/" +
-      str.substring(p1 + 1, p2) +
-      "-pop" +
-      str.substring(p2)
-    openShadowBox(str)
-  })
+  // $(".scrollbox ul li:not(:first-child) img").click(function (e) {
+  //   var str = e.target.src
+  //   var p1 = str.lastIndexOf("/")
+  //   var p2 = str.lastIndexOf(".")
+  //   str =
+  //     str.substring(0, p1 + 1) +
+  //     "pop1/" +
+  //     str.substring(p1 + 1, p2) +
+  //     "-pop" +
+  //     str.substring(p2)
+  //   openShadowBox(str)
+  // })
 
   $(".scrollbox-7 ul li img").click(function (e) {
     var str = e.target.src
@@ -113,7 +115,7 @@ $(function () {
   })
 
   $(
-    ".c1, .c2, .c3, .c4, .c5, .c6, .d1, .d2, .d3, .d4, .offre1, .offre2, .offre3, .offre4, .h1, .h2, .h3, .h4, .p1, .p2, .p3, .p4, .p5 , .p6, .p7, .p8, .p9, .p10, .p11, .p12 , .s1, .s2, .s3, .s4, .s5 , .s6, .s7, .s8, .s9, .s10, .s11, .s12, .s13, .s14, .s15,.s16, .s17, .s18,.s19, .s20, .s21, .s22, .s23, .s24, .s25, .s26, .s27, .s28, s29"
+    ".c1, .c2, .c3, .c4, .c5, .c6, .offre1, .offre2, .offre3, .offre4, .h1, .h2, .h3, .h4, .p1, .p2, .p3, .p4, .p5 , .p6, .p7, .p8, .p9, .p10, .p11, .p12 , .s1, .s2, .s3, .s4, .s5 , .s6, .s7, .s8, .s9, .s10, .s11, .s12, .s13, .s14, .s15,.s16, .s17, .s18,.s19, .s20, .s21, .s22, .s23, .s24, .s25, .s26, .s27, .s28, s29"
   ).click(function (e) {
     var bg = $(this).css("background-image")
     bg = bg.replace("url(", "").replace(")", "").replace(/\"/gi, "")
@@ -122,7 +124,6 @@ $(function () {
   $(".shadow").click(closeShadowBox)
   $(".shadow").click(closeVideoShadowBox)
 })
-
 
 //
 // functions for page handling
